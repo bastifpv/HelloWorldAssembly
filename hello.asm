9277f8c1160fdbe5bae9af55d1e6d477361cd718
Bastian Reuther <bastir25@icloud.com>
2023-11-28 23:42:12 +0100

    add input read and print

--- hello.asm
+++ hello.asm
@@ -9,10 +9,24 @@ _start:
     MOV rdx, msglen
     SYSCALL
 
+    MOV rax, 0x0
+    MOV rdi, 0x0
+    MOV rsi, dynvar
+    MOV rdx, dynvarlen
+    SYSCALL
+
+    MOV rax, 0x1
+    MOV rdi, 0x1
+    MOV rsi, dynvar
+    MOV rdx, dynvarlen
+    SYSCALL
+
     MOV rax, 0x3C
     MOV rdi, 0x0
     SYSCALL
 
 section .data
     msg: DB "Hello, World", 0xA
-    msglen: EQU $ - msg;   
+    msglen: EQU $ - msg
+    dynvar: DB "Test"
+    dynvarlen: EQU 0x20 
